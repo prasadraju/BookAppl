6693d91ede234f27019c1f529fef97f6a15e2d97
prasadraju <prasadrajukonduru@gmail.com>
2019-01-27 11:48:07 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
+import { NgModule }   from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { FormsModule }   from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { PageNotFoundComponent }  from './page-not-found.component';
+import { HomeComponent }  from './home/home.component';
+import { ViewDetailComponent }  from './home/view-detail.component';
+import { AddBookComponent }  from './add-book/add-book.component';
+import { UpdateBookComponent }  from './manage-book/update-book.component';
+import { ManageBookComponent }  from './manage-book/manage-book.component';
+import { BookService } from './services/book.service';
+import { AppRoutingModule }  from './app-routing.module';
 
 @NgModule({
+  imports: [     
+        BrowserModule,
+		FormsModule,
+		AppRoutingModule
+  ],
   declarations: [
-    AppComponent
+        AppComponent, 
+		PageNotFoundComponent,
+		HomeComponent,
+		ViewDetailComponent,
+		AddBookComponent,
+		ManageBookComponent,
+		UpdateBookComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ BookService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
